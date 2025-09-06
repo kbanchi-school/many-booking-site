c428543b4956927a2a56294693b58437621dc209
kbanchi1111 <kbanchi.1111@gmail.com>
2025-09-06 13:59:05 +0900

    add liff init

--- blueprints/auth/app_auth.py
+++ blueprints/auth/app_auth.py
@@ -1,6 +1,14 @@
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
 from flask import render_template, request, redirect, url_for
 from . import auth_bp
 
+LIFF_ID = os.getenv("LIFF_ID")
+ENV = os.getenv("ENV")
+
 @auth_bp.route('/login')
 def login():
-    return render_template('auth/login.html')
+    return render_template('auth/login.html', liff_id=LIFF_ID, env=ENV)
