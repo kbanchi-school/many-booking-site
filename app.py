d8b4762a330be221b03dee3a749ad2105bdd9721
kbanchi1111 <kbanchi.1111@gmail.com>
2025-09-07 17:21:51 +0900

    kbanchi

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def hello():
     return render_template('hello.html')
 
+@app.route('/kazunori')
+def kazunori():
+    return render_template('kazunori.html')
+
+@app.route('/aoi')
+def aoi():
+    return render_template('aoi.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
