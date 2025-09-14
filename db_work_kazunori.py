f04caeef5926e9bbd9a4a1b69b7c5744f5eb96bb
tagutikazunori <tagutikazunori@gmail.com>
2025-09-14 17:16:10 +0900

    「これからプッシュしようと思います。」

--- db_work_kazunori.py
+++ db_work_kazunori.py
@@ -1,1 +1,11 @@
 # かずのり作業用
+
+from database import *
+
+rituki = Person(name='高宮律樹', age=12)
+keito = Person(name='齋藤慶人', age=13)
+kouto = Person(name='熊谷洸人', age=13)
+
+rituki.save()
+keito.save()
+kouto.save()
