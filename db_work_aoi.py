82c98d48508ab64bb8839c4bb51afc05913a3168
azinorishironingen <aoi.taguchi.m@gmail.com>
2025-09-14 17:07:59 +0900

    add test data for Person

--- db_work_aoi.py
+++ db_work_aoi.py
@@ -1,1 +1,11 @@
 # あおい作業用
+
+from database import *
+
+shironingen = Person(name='しろにんげん', age=32)
+okazu = Person(name='おかず担当大臣', age=20)
+hohoemi = Person(name="ほほえみさん",age = 36)
+
+shironingen.save()
+okazu.save()
+hohoemi.save()
