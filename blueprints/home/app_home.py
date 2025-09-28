3e316f510153eb71e92c83f069ecd32c90e9b99f
azinorishironingen <aoi.taguchi.m@gmail.com>
2025-09-28 17:19:10 +0900

    home

--- blueprints/home/app_home.py
+++ blueprints/home/app_home.py
@@ -1,6 +1,9 @@
 from flask import render_template, request, redirect, url_for
 from . import home_bp
 
+from database import Salon
+
 @home_bp.route('/')
 def home():
-    return render_template('home.html')
+    salons = Salon.select()
+    return render_template('home.html', salons=salons)
--- blueprints/info/app_info.py
+++ blueprints/info/app_info.py
@@ -1,6 +1,9 @@
 from flask import render_template, request, redirect, url_for
 from . import info_bp
 
+from database import Notification
+
 @info_bp.route('/')
 def info():
-    return render_template('info.html')
+    notifications = Notification.select()
+    return render_template('info.html', notifications=notifications)
