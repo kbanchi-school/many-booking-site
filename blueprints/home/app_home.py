ecd5fbe08c1498991261546edc27f509e1bb3cbd
kbanchi1111 <kbanchi.1111@gmail.com>
2025-10-05 23:16:44 +0900

    fix home

--- blueprints/home/app_home.py
+++ blueprints/home/app_home.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import render_template, request, redirect, url_for
 from . import home_bp
 
@@ -5,10 +7,9 @@ from database import Salon , Service , Address ,WorkingHour
 
 @home_bp.route('/')
 def home():
-    salons = Salon.select()
-    address = Address.select()
-    workinghours = WorkingHour.select()
-    return render_template('home.html', salons=salons, address=address, workinghours=workinghours)
+    today_weekday = datetime.now().weekday()
+    addresses = Address.select(Address, Salon, WorkingHour).join(Salon).join(WorkingHour).where(WorkingHour.weekday == today_weekday)
+    return render_template('home.html', addresses=addresses)
 
 @home_bp.route('/detail/<id>')
 def detail(id):
