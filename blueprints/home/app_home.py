f10261825ae6f63be26fe56a2666405acbafd3fb
azinorishironingen <aoi.taguchi.m@gmail.com>
2025-10-05 17:26:20 +0900

    a

--- blueprints/home/app_home.py
+++ blueprints/home/app_home.py
@@ -1,14 +1,19 @@
 from flask import render_template, request, redirect, url_for
 from . import home_bp
 
-from database import Salon
+from database import Salon , Service , Address ,WorkingHour
 
 @home_bp.route('/')
 def home():
     salons = Salon.select()
-    return render_template('home.html', salons=salons)
+    address = Address.select()
+    workinghours = WorkingHour.select()
+    return render_template('home.html', salons=salons, address=address, workinghours=workinghours)
 
 @home_bp.route('/detail/<id>')
 def detail(id):
     salon = Salon.get(Salon.id == id)
-    return render_template('home_detail.html', salon=salon)
+    services = Service.select().where(Service.salon == salon)
+
+
+    return render_template('home_detail.html', salon=salon, services=services)
