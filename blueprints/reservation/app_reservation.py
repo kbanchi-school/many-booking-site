5e3d881d06932732b1fc99c61380bedac51be50f
tagutikazunori <tagutikazunori@gmail.com>
2025-11-23 11:29:06 +0900

    予約一覧の細部を心を込めて変更させていただきました。

--- blueprints/reservation/app_reservation.py
+++ blueprints/reservation/app_reservation.py
@@ -1,8 +1,9 @@
 from flask import render_template, request, redirect, url_for
 from . import reservation_bp
 from database import Reservation
+from database import Salon
 
 @reservation_bp.route('/')
 def reservation():
-    reservations = Reservation.select()
+    reservations = Reservation.select(Reservation,Salon).join(Salon)
     return render_template('reservation.html',reservations=reservations)
