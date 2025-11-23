e2b2abd7ca8c1c9f62f625522af923e8531c34c1
tagutikazunori <tagutikazunori@gmail.com>
2025-11-23 10:29:42 +0900

    田口和典

--- blueprints/reservation/app_reservation.py
+++ blueprints/reservation/app_reservation.py
@@ -1,6 +1,8 @@
 from flask import render_template, request, redirect, url_for
 from . import reservation_bp
+from database import Reservation
 
 @reservation_bp.route('/')
 def reservation():
-    return render_template('reservation.html')
+    reservations = Reservation.select()
+    return render_template('reservation.html',reservations=reservations)
