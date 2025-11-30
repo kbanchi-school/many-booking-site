2187f9f33eb264b6b63b6931271a3032ae491302
azinorishironingen <aoi.taguchi.m@gmail.com>
2025-11-30 10:20:01 +0900

    エラーきえました多分

--- blueprints/home/app_home.py
+++ blueprints/home/app_home.py
@@ -35,5 +35,12 @@ def detail(id):
     services = Service.select().where(Service.salon == salon)
     workinghour = WorkingHour.get(WorkingHour.id == id)
 
-
     return render_template('home_detail.html', salon=salon, services=services ,workinghour=workinghour)
+
+@home_bp.route('/detail/<int:salon_id>/<int:service_id>/calendar')
+def detail(id):
+    salon = Salon.get(Salon.id == id)
+    services = Service.select().where(Service.salon == salon)
+    workinghour = WorkingHour.get(WorkingHour.id == id)
+    
+    return render_template('home_calendar.html',salon=salon, services=services ,workinghour=workinghour)
