8909220ceda642ba3e590b4bcd55dfa0f9a1ce3e
azinorishironingen <aoi.taguchi.m@gmail.com>
2025-12-21 18:12:49 +0900

    　未読既読の絞り込み

--- blueprints/info/app_info.py
+++ blueprints/info/app_info.py
@@ -5,8 +5,15 @@ from database import Notification
 
 @info_bp.route('/')
 def info():
-    notifications = Notification.select()
-    return render_template('info.html', notifications=notifications)
+    isread = request.args.get("is_read")
+
+    query = Notification.select()
+
+    if isread in ("0", "1"):
+        query = query.where(Notification.is_read == int(isread))
+
+    return render_template('info.html', notifications=query)
+
 
 @info_bp.route('/read/<id>', methods=['POST'])
 def info_read(id):
